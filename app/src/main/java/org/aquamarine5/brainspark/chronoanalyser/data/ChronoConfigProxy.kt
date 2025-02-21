2b2eb0c69e65bd982529de8378d12af7561d45f6
aquamarine5 <3168287806@QQ.com>
2025-02-21 13:44:22 +0800

    Update ChronoConfigProxy.kt

--- app/src/main/java/org/aquamarine5/brainspark/chronoanalyser/data/ChronoConfigProxy.kt
+++ app/src/main/java/org/aquamarine5/brainspark/chronoanalyser/data/ChronoConfigProxy.kt
@@ -6,7 +6,26 @@ class ChronoConfigProxy<T>(
     private val sharedPreferences: SharedPreferences,
     private val key: String,
     private val defaultValue: T
-){
+) {
+    @Suppress("UNCHECKED_CAST")
+    fun isDefaultValue(): Boolean {
+        return when (defaultValue) {
+            is Long -> sharedPreferences.getLong(key, defaultValue) as T == defaultValue
+            is String -> sharedPreferences.getString(key, defaultValue) as T == defaultValue
+            is Int -> sharedPreferences.getInt(key, defaultValue) as T == defaultValue
+            is Boolean -> sharedPreferences.getBoolean(key, defaultValue) as T == defaultValue
+            is Float -> sharedPreferences.getFloat(key, defaultValue) as T == defaultValue
+            else -> throw IllegalArgumentException("Unsupported type")
+        }
+    }
+
+    fun clear() {
+        with(sharedPreferences.edit()) {
+            remove(key)
+            apply()
+        }
+    }
+
     @Suppress("UNCHECKED_CAST")
     fun getValue(): T {
         return when (defaultValue) {
